e7245bf6f64a700e70f724b4a5a1cd665383c808
Chervyakov Artem <artem9617@gmail.com>
2014-10-11 23:06:33 +0400

    test
    test commit

---
+++ BlockTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+	public class BlockTest {
+
+		public static void main(String[] args) {
+			
+
+			int x, y ;
+
+			y = 20 ;
+
+			for(x=0; x < 10; x++){
+
+				System.out.println("Znachenie x:" + x);
+
+				System.out.println("Znachenie y:" +y);
+
+				y = y - 2;
+			}
+		}
+
+}
